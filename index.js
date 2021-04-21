e50135ffb53da3719fef0d7a676e9ef2509f8d93
Moulham Hallak <mou990@gmail.com>
2021-04-21 22:34:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
-companyName = 'Chelsea';
-companyName = "Susan Smith";
+// companyName = 'Chelsea';
+// companyName = "Susan Smith";
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = "Susan Smith";
