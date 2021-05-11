7fe53f67d445e951e9e012dab311c700eebe81d0
iseluin <kutay.kagan.ozenn@gmail.com>
2021-05-11 23:18:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+
+companyName = "DifName";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+mostProfitableNeighborhood = "London";
+
+let CompanyCeo = "Kutay";
+
+CompanyCeo = "Kağan";
